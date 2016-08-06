ea5c569d7349d63db3e5af5ad3c6e8d58956b7c8
Ian Rust <iancrust@gmail.com>
2016-08-06 14:08:48 -0700

    compiles

--- bloons/bloons.ino
+++ bloons/bloons.ino
@@ -6,17 +6,35 @@
 #include <Wire.h>
 #include <Adafruit_MMA8451.h>
 #include <Adafruit_Sensor.h>
-#include <stdint.h>
-#include "Balloon.h"
+#include <acceled.h>
 
-Balloon *b;
+AcceLED al1 = AcceLED(5, 5);
 
-void setup() {
-  Serial.begin(9600);
-  b = &Balloon(12, 5);
-  b->setColor(200,10,10);
+void setup()
+{
+  al1.begin();
 }
 
-void loop() {
-  b->update();  
+boolean toggle1 = false;
+unsigned long toggle_millis1 = 0;
+
+void loop()
+{
+  float mag1 = al1.accelMagnitude();
+  float angle1 = 500*al1.angleFromVertical();
+  
+  if (mag1 > 15 && millis()-toggle_millis1 > 1000)
+  {
+    toggle1 = !toggle1;
+    toggle_millis1 = millis();
+  }
+  
+  if (toggle1)
+  {
+    al1.setSolid(0,170,min(170,angle1));
+  }
+  else
+  {
+    al1.setSolid(170,0,min(170,angle1));
+  }
 }
